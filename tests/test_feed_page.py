2adc1f898a08c1cc6e9984806a4822c46e4b26fa
Kirill Ulyanov <ulyanov.k.mgn@gmail.com>
2025-04-14 19:51:04 +0300

    v2

--- tests/test_feed_page.py
+++ tests/test_feed_page.py
@@ -4,15 +4,16 @@ from pages.account_page import AccountPage
 from pages.main_page import MainPage
 import allure
 
+
 class TestFeedOrdersPage:
 
     @allure.title('Проверка перехода к деталям заказа по клику на заказ в Ленте заказов')
     def test_check_open_order_details_by_feed_button(self, driver):
         feed_page = FeedPage(driver)
         feed_page.open_feed_page()
-        
+
         assert feed_page.click_on_order_block()
-        
+
     @allure.title('Проверка синхронизации раздела История заказов с Лента заказов')
     def test_check_sync_history_order_and_feed_orders(self, driver, login_user_and_create_order):
         login_page = LoginPage(driver)
@@ -25,46 +26,63 @@ class TestFeedOrdersPage:
         feed_page = FeedPage(driver)
         feed_page.open_feed_page()
         number_in_feed = feed_page.check_number_order_in_feed_page(number)
-        
+
         assert number == number_in_history and number_in_feed == True
-        
+
     @allure.title('Проверка увеличения счетчика Выполнено за всё время после создания заказа')
     def test_check_rise_counter_all_time_after_made_order(self, driver, create_user):
         feed_page = FeedPage(driver)
         feed_page.open_feed_page()
-        counter_before = feed_page.get_counter_all_time_text()
+
+        # Получаем текущее значение счетчика и преобразуем его в целое число
+        counter_before = int(feed_page.get_counter_all_time_text())
+
         login_page = LoginPage(driver)
         login_page.open_login_page()
         login_page.user_login(create_user)
+
         main_page = MainPage(driver)
         main_page.create_order_process()
+
         feed_page.open_feed_page()
-        counter_after = feed_page.get_counter_all_time_text()
-        
-        assert counter_before != counter_after
-        
+
+        # Получаем новое значение счетчика и преобразуем его в целое число
+        counter_after = int(feed_page.get_counter_all_time_text())
+
+        assert counter_after > counter_before  # Проверяем, что счетчик увеличился
+
     @allure.title('Проверка увеличения счетчика Выполнено за сегодня после создания заказа')
     def test_check_rise_counter_today_after_made_order(self, driver, create_user):
         feed_page = FeedPage(driver)
         feed_page.open_feed_page()
-        counter_before = feed_page.get_counter_today_text()
+
+        # Получаем текущее значение счетчика и преобразуем его в целое число
+        counter_before = int(feed_page.get_counter_today_text())
+
         login_page = LoginPage(driver)
         login_page.open_login_page()
         login_page.user_login(create_user)
+
         main_page = MainPage(driver)
         main_page.create_order_process()
+
         feed_page.open_feed_page()
-        counter_after = feed_page.get_counter_today_text()
-        
-        assert counter_before != counter_after
-        
+
+        # Получаем новое значение счетчика и преобразуем его в целое число
+        counter_after = int(feed_page.get_counter_today_text())
+
+        assert counter_after > counter_before  # Проверяем, что счетчик увеличился
+
     @allure.title('Проверка наличия номера заказа в статусе В работе после его создания')
     def test_check_order_status_in_work_after_order_creation(self, driver, login_user_and_create_order):
         login_page = LoginPage(driver)
         login_page.open_login_page()
+
         number = login_page.user_login_with_order(login_user_and_create_order)
+
         feed_page = FeedPage(driver)
         feed_page.open_feed_page()
+
         number_in_at_work = feed_page.check_number_order_in_at_work()
-        
-        assert number == number_in_at_work        
+
+        assert number == number_in_at_work
